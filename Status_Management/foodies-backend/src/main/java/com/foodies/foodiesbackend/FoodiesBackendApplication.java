cd8d546170b7916f7572ec0caad0fa2dfa26b9cc
Premakumara-YKV IT20252854 <100156811+IT20252854@users.noreply.github.com>
2023-05-04 20:41:02 +0530

    Create spring boot for backend

---
+++ Status_Management/foodies-backend/src/main/java/com/foodies/foodiesbackend/FoodiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodies.foodiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiesBackendApplication.class, args);
+	}
+
+}
