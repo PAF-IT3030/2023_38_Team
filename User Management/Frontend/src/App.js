9e25aa3065779b87e83ad857a9b3f0e65f2b2981
Kalpana Fernando <101004190+KalpanaFernando@users.noreply.github.com>
2023-05-07 12:36:40 +0530

    Create Frontend

---
+++ User Management/Frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './Components/LoginPage/LoginPage';
+import Home from './Components/HomePage/Home';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <LoginPage / > */}
+
+       <Home/> 
+    </div>
+  );
+}
+
+export default App;
