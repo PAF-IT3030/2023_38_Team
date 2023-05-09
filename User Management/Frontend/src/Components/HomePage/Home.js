f4ffd9289f75ba20814beb941a8d545b82aa664e
Kalpana Fernando <101004190+KalpanaFernando@users.noreply.github.com>
2023-05-09 20:19:05 +0530

    Update Home.js

--- User Management/Frontend/src/Components/HomePage/Home.js
+++ User Management/Frontend/src/Components/HomePage/Home.js
@@ -3,7 +3,7 @@ import "./Home.css"
 import NavBar from '../NavBar/NavBar';
 import MainContent from '../MainContent/MainContent';
 
-class Home extends  {
+class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
