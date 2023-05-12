3d62d5180a3881875b7bb75b35cfbce8f62dca19
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2023-05-12 09:45:49 +0530

    creating React List Comment Component

--- Comment_Management/foodies-frontend/src/Components/MainPage/MainPage.js
+++ Comment_Management/foodies-frontend/src/Components/MainPage/MainPage.js
@@ -1,127 +1,125 @@
-import React, { Component } from 'react';
-import "./MainPage.css";
-import Post from '../Post/Post';
-import uploadImage from "../../images/upload.png";
-// import {storage,auth} from "../firebase";
+import { Avatar, Box, Button, Divider, Grid, IconButton, TextField, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import SendIcon from '@mui/icons-material/Send';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import axios from "axios";
+import "./CommentsList.css";
+import { useNavigate } from 'react-router-dom';
 
 
-class MainPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            postArray: [],
-            progressBar: "",
-        }
-    }
 
-    componentDidMount() {
-        this.getPost();
-    }
+return (
+    <Grid flexGrow={1}>
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={12} md={12} lg={12} className="comment-toolbar">
 
-    getPost = () => { //API
-        const thisContext = this;
-        let data = [
-            {
-                "postId": "123456",
-                "userName": "anindya",
-                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
-                "timeStamp": "12345",
-                "likes": "1234"
-            },
-            {
-                "postId": "123456",
-                "userName": "anindya",
-                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
-                "timeStamp": "12345",
-                "likes": "1234"
-            },
-            {
-                "postId": "123456",
-                "userName": "anindya",
-                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
-                "timeStamp": "12345",
-                "likes": "1234"
-            }
-        ];
-        thisContext.setState({ postArray: data });
+            </Grid>
+        </Grid>
+        <Grid flexGrow={1}>
+            <Box
+                mb={8}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                }}
+            >
+                <Avatar sx={{ marginLeft: "140px" }} alt="Remy Sharp" src={"#"} />
 
-        // fetch('http://localhost:8080/post')
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         thisContext.setState({postArray: data});
-        // });
-    }
+                <TextField
+                    id="standard-basic"
+                    placeholder='Add a comment...'
+                    color="secondary"
+                    inputProps={ariaLabel}
 
-    // upload=(event)=>{
-    //     let image=event.target.files[0];
-    //     const thisContext=this;
-    //     if(image == null || image == undefined)
-    //         return;
+                    variant="standard"
+                    onChange={handleChange}
+                    value={comment}
+                    style={{ width: '50%', marginLeft: '30px' }}
+                    // inputProps={{}}
+                    onKeyPress={(ev) => {
+                        if (ev.key === 'Enter') {
+                            ev.preventDefault();
+                            sendComment()
+                            setComment('')
+                        }
+                    }}
+                />
 
-    //     var uploadTask = storage.ref("images").child(image.name).put(image);
-    //     uploadTask.on(
-    //       "state_changed",
-    //       function (snapshot) {
-    //         var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //         thisContext.setState({progressBar: progress});
-    //       },
-    //       function (error) {
-    //       },
-    //       function () {
-    //         uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-    //             console.log(downloadURL);
 
-    //             let payload = {
-    //                 "postId": Math.floor(Math.random()*100000).toString(),
-    //                 "userId": JSON.parse(localStorage.getItem("users")).uid,
-    //                 "postPath": downloadURL,
-    //                 "timeStamp": new Date().getTime(),
-    //                 "likeCount": 0
-    //             }
+                <Button className="comment-button" backgroundColor='primary'
+                    onClick={(event) => {
+                        event.preventDefault();
+                        sendComment()
+                        setComment('')
+                    }}>
+                    <Typography sx={{ color: 'white' }}><SendIcon color='secondary' /></Typography>
+                </Button>
+            </Box>
 
-    //             const requestOptions ={
-    //                 method: "POST",
-    //                 headers: { 'Content-Type': 'application/json' },
-    //                 body : JSON.stringify(payload),
-    //             }
+            <Box>
 
-    //             fetch("http://localhost:8080/post",requestOptions)
-    //             .then(response => response.json())
-    //             .then(data => {
-    //                 console.log(data);
-    //                 thisContext.getPost();
-    //             })
-    //             .catch(error =>{
+                {myComments && myComments.map((item, index) => (
+                    <Box sx={{ marginLeft: "80px", display: 'flex', alignItems: 'center' }} key={index} mb={3}>
+                        <Box><Avatar src={item.picture} alt="Remy Sharp" sx={{ height: 50, width: 50 }} /></Box>
 
-    //             })
+                        <Box className="comment-text" >
+                            <Box display={"flex"} gap={"20px"}>
+                                <Typography variant="subtitle1" gutterBottom component="div" sx={{ fontSize: '14px', fontWeight: '550' }}>
+                                    me
+                                </Typography>
 
-    //         })
-    //         }
-    //     );
-    // }
+                                <Typography variant="subtitle1" gutterBottom component="div" sx={{ color: 'gray', fontSize: '13px', fontWeight: '300' }}>
+                                    5 minutes ago
+                                </Typography>
+                            </Box>
 
-    render() {
-        return (
-            <div>
-                <div className="mainpage__container">
-                    <div className="mainpage__divider"></div>
-                    <div className="fileupload">
-                        <label for="file-upload" >
-                            <img className="mainpage__uploadicon" src={uploadImage} />
-                        </label>
-                        <input onChange={this.upload} id="file-upload" type="file" />
-                    </div>
-                    <div className="mainpage__divider"></div>
-                </div>
-                <div className="upload_text">{this.state.progressBar}</div>
-                {
-                    this.state.postArray.map((item, index) => (
-                        <Post id={item.postId} userName={item.userName} postImage={item.postImageURL} likes={item.likes} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+                            <Typography variant="body2" gutterBottom component="div" sx={{ fontSize: '14px', fontWeight: '500' }}>
+                                {item.comment}
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <IconButton sx={{ backgroundColor: "orange", marginRight: 1 }} component="label" onClick={() => handleUpdate(item.cid)} >
+                                <ModeEditIcon />
+                            </IconButton>
+                            <IconButton sx={{ backgroundColor: "red" }} component="label" onClick={() => handleDelete(item.cid)}>
+                                <DeleteIcon />
+                            </IconButton>
+                        </Box>
 
-export default MainPage;
+
+                    </Box>
+                ))}
+
+                {commentData && commentData.map((item, index) => (
+                    <Box sx={{ marginLeft: "80px", display: 'flex', alignItems: 'center' }} key={index} mb={3}>
+                        <Box><Avatar src={item.picture} alt="Remy Sharp" sx={{ height: 50, width: 50 }} /></Box>
+
+                        <Box className="comment-text" >
+                            <Box display={"flex"} gap={"20px"}>
+                                <Typography variant="subtitle1" gutterBottom component="div" sx={{ fontSize: '14px', fontWeight: '550' }}>
+                                    {item.username}
+                                </Typography>
+
+                                <Typography variant="subtitle1" gutterBottom component="div" sx={{ color: 'gray', fontSize: '13px', fontWeight: '300' }}>
+                                    {item.time}
+                                </Typography>
+                            </Box>
+
+                            <Typography variant="body2" gutterBottom component="div" sx={{ fontSize: '14px', fontWeight: '500' }}>
+                                {item.comment}
+                            </Typography>
+                        </Box>
+
+                    </Box>
+                ))}
+
+            </Box>
+
+        </Grid>
+    </Grid>
+)
+
+
+
+export default CommentList;
